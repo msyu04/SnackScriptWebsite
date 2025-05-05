ac7019b13ccb1fd77657e084bde0ba1db6fd3718
Matthew De Jesus <msyu04@gmail.com>
2025-05-05 15:37:40 -0700

    added easter egg

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("SnackScript website loaded!");
 });
+
+document.getElementById("easterEggBtn").addEventListener("click", function() {
+    const text = document.getElementById("easterEggText");
+    text.classList.toggle("hidden");
+});
